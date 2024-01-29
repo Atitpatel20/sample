377963446b5747645d7a8c895912eb8fea12f226
Atit Patel <atitpatel296@gmail.com>
2024-01-29 14:38:33 +0530

    29.01.24

---
+++ src/main/java/com/myblog/myblog11/Test.java
@@ -0,0 +1,12 @@
+package com.myblog.myblog11;
+
+public class Test {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.returnOtp();
+    }
+
+    private int returnOtp() {
+        return 256314;
+    }
+}
